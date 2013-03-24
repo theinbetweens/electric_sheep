06fb87b8a4339180eb9e063d61c8ab99742782ac
theinbetweens <wsangster@theinbetweens.co.uk>
2013-03-24 13:03:34 +0000

    Updates to generate file

--- lib/electric_sheep/models/acts_as_story.rb
+++ lib/electric_sheep/models/acts_as_story.rb
@@ -9,6 +9,10 @@ module ElectricSheep
 
       def build_tads
         str = []
+
+        str << '#include <adv3.h>'
+        str << '#include <en_us.h>'
+
         str << 'gameMain: GameMainDef'
         str << "  initialPlayerChar = me"
         str << ';'
@@ -25,15 +29,28 @@ module ElectricSheep
           output += "\n\n"
           output += actors.first.write
         end
+
+        # TODO This a quick hack so it can compile
+        output += "\n"
+        output += 'me: Actor'
+        output += "\n"
+        output += ';'
+
         output
       end   
 
       def t_file_name
-        name.downcase.gsub(/\s+/,'-')+'.t'
+        #name.downcase.gsub(/\s+/,'')+'.t'
+        name.split(/\s+/).collect(&:capitalize).join('')+'.t'
       end   
 
       def t3m_file_name
-        name.downcase.gsub(/\s+/,'-')+'.t3m'
+        #name.downcase.gsub(/\s+/,'')+'.t3m'
+        name.split(/\s+/).collect(&:capitalize).join('')+'.t3m'
+      end
+
+      def camelcase_name
+        name.split(/\s+/).collect(&:capitalize).join('')
       end
 
       def t3m_file_content
@@ -41,11 +58,38 @@ module ElectricSheep
         str << '-D LANGUAGE=en_us'
         str << '-D MESSAGESTYLE=neu'
         str << '-Fy obj -Fo obj'
-        str << "-o #{name}.t3"
+        str << "-o #{camelcase_name}.t3"
         str << '-lib system'
         str << '-lib adv3/adv3'
-        str << "-source #{name}"
+        str << "-source #{camelcase_name}"
         str.join("\n")
+      end
+
+      def game_info_output
+        # IFID: b8563851-6257-77c3-04ee-278ceaeb48ac
+        # Name: My First Game
+        # Byline: by Bob Author
+        # AuthorEmail: Bob Author <bob@myisp.com>
+        # Desc: This is an example of how to start a new game project. 
+        # Version: 1
+        # ReleaseDate: 2013-01-27
+        # Language: en-US
+        # LicenseType: Freeware
+        # CopyingRules: Nominal cost only; compilations allowed
+        # PresentationProfile: Default                
+        str = []
+        str << 'IFID: b8563851-6257-77c3-04ee-278ceaeb48ac'
+        str << 'Name: My First Game'
+        str << 'Byline: by Bob Author'
+        str << "AuthorEmail: Bob Author <bob@myisp.com>"
+        str << 'Desc: This is an example of how to start a new game project.'
+        str << 'Version: 1'
+        str << "ReleaseDate: 2013-01-27"
+        str << "Language: en-US"
+        str << "LicenseType: Freeware"
+        str << "CopyingRules: Nominal cost only; compilations allowed"
+        str << "PresentationProfile: Default"
+        str.join("\n")        
       end
 
     end
--- spec/electric_sheep/game_spec.rb
+++ spec/electric_sheep/game_spec.rb
@@ -9,15 +9,43 @@ module ElectricSheep
       it 'should contain the author information' do
         ElectricSheep::Game.new(:name => 'Blade Runner').build_tads.should include("name = 'Blade Runner'")
       end
+
+      it 'should include the standard phython includes' do
+         #include <adv3.h>
+     #include <en_us.h>
+        ElectricSheep::Game.new(:name => 'Blade Runner').build_tads.should include("#include <adv3.h>")
+        ElectricSheep::Game.new(:name => 'Blade Runner').build_tads.should include("#include <en_us.h>")
+      end
       
     end
 
-    describe '.build' do
+    # describe '.build' do
 
-    	it 'should generate a game directory on build' do
-    		ElectricSheep::Game.new(:name => 'Blade Runner').build
-    		File.exists?('BladeRunner').should == true
-    	end
+    # 	it 'should generate a game directory on build' do
+    # 		ElectricSheep::Game.new(:name => 'Blade Runner').build
+    # 		File.exists?('BladeRunner').should == true
+    # 	end
+
+    # end
+
+    describe '.game_info_output' do
+
+      it 'should generate the standard output for the GameInfo.txt' do
+        # IFID: b8563851-6257-77c3-04ee-278ceaeb48ac
+        # Name: My First Game
+        # Byline: by Bob Author
+        # AuthorEmail: Bob Author <bob@myisp.com>
+        # Desc: This is an example of how to start a new game project. 
+        # Version: 1
+        # ReleaseDate: 2013-01-27
+        # Language: en-US
+        # LicenseType: Freeware
+        # CopyingRules: Nominal cost only; compilations allowed
+        # PresentationProfile: Default        
+        ElectricSheep::Game.new(:name => 'Blade Runner').game_info_output.should include("AuthorEmail: Bob Author <bob@myisp.com>")
+        ElectricSheep::Game.new(:name => 'Blade Runner').game_info_output.should include("Language: en-US")
+        ElectricSheep::Game.new(:name => 'Blade Runner').game_info_output.should include("PresentationProfile: Default")
+      end
 
     end
 
@@ -25,7 +53,7 @@ module ElectricSheep
 
       it 'should return a file name based on the story name' do
         game = ElectricSheep::Game.new(:name => 'Blade Runner')
-        game.t_file_name.should == 'blade-runner.t'
+        game.t_file_name.should == 'BladeRunner.t'
       end
 
     end
@@ -35,7 +63,7 @@ module ElectricSheep
 
       it 'should return a valid *.t3m file name' do
         game = ElectricSheep::Game.new(:name => 'Blade Runner')
-        game.t3m_file_name.should == 'blade-runner.t3m'      
+        game.t3m_file_name.should == 'BladeRunner.t3m'      
       end
 
     end
@@ -44,13 +72,13 @@ module ElectricSheep
 
       it 'should contain t3m values' do
         # TODO Need nicer way to compare generated files
-        ElectricSheep::Game.new(:name => 'MyGame').t3m_file_content.should include("-D LANGUAGE=en_us")
-        ElectricSheep::Game.new(:name => 'MyGame').t3m_file_content.should include("-D MESSAGESTYLE=neu")
-        ElectricSheep::Game.new(:name => 'MyGame').t3m_file_content.should include("-Fy obj -Fo obj")
-        ElectricSheep::Game.new(:name => 'MyGame').t3m_file_content.should include("-o MyGame.t3")     
-        ElectricSheep::Game.new(:name => 'MyGame').t3m_file_content.should include("-lib system")
-        ElectricSheep::Game.new(:name => 'MyGame').t3m_file_content.should include("-lib adv3/adv3")
-        ElectricSheep::Game.new(:name => 'MyGame').t3m_file_content.should include("-source MyGame")    
+        ElectricSheep::Game.new(:name => 'My Game').t3m_file_content.should include("-D LANGUAGE=en_us")
+        ElectricSheep::Game.new(:name => 'My Game').t3m_file_content.should include("-D MESSAGESTYLE=neu")
+        ElectricSheep::Game.new(:name => 'My Game').t3m_file_content.should include("-Fy obj -Fo obj")
+        ElectricSheep::Game.new(:name => 'My Game').t3m_file_content.should include("-o MyGame.t3")     
+        ElectricSheep::Game.new(:name => 'My Game').t3m_file_content.should include("-lib system")
+        ElectricSheep::Game.new(:name => 'My Game').t3m_file_content.should include("-lib adv3/adv3")
+        ElectricSheep::Game.new(:name => 'My Game').t3m_file_content.should include("-source MyGame")    
       end
 
       # """
